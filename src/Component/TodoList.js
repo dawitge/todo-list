a49aa7ff55d09e9d7981d849e2e8d2f8f7a9e153
Dawit Geremew <dawitge@gmail.com>
2020-09-20 20:34:24 +0300

    first commit

---
+++ src/Component/TodoList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import TodoItem from './TodoItem';
+export default class TodoList extends Component {
+    render() {
+        return (
+            <div>
+                <h1>TodoInput</h1>
+                <div className='col-4 bg-warning'>hello</div>
+                <div className='col-4 bg-danger'>hello</div>
+                <div className='col-4 bg-primary'>hello</div>
+            </div>
+        )
+    }
+}
